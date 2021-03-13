c32658b6f0b0b9375e87294af6606049d7ea7773
tanavieivnewhope <minimini_couper_0927@yahoo.co.jp>
2021-03-13 19:00:00 +0900

    20210313

---
+++ js/content/20210313.js
@@ -0,0 +1,19 @@
+function modalImageToggle(idx) {
+    const targetElm = document.getElementsByClassName('modal-box');
+  
+    targetElm[idx].classList.toggle('modal-on');
+  }
+  
+  function modalImageOff(idx) {
+    const targetElm = document.getElementsByClassName('modal-box');
+  }
+  
+  const targetScreenElm = document.getElementsByClassName('screen_shot');
+  for(let idx = 0; idx < targetScreenElm.length; idx++){
+    targetScreenElm[idx].addEventListener('click',() => modalImageToggle(idx));
+  }
+  
+  const targetCloseElm = document.getElementsByClassName('modal-box__close');
+  for(let idx = 0; idx < targetCloseElm.length; idx++){
+    targetCloseElm[idx].addEventListener('click',() => modalImageToggle(idx));
+  }
